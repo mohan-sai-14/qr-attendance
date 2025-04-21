c584c4bf4a7cd438ee9bebc994818698e268d97b
Mohan sai reddy <mohansaireddy22@gmail.com>
2025-04-21 19:26:17 +0530

    Export User interface from auth.tsx

--- app/client/src/lib/auth.tsx
+++ app/client/src/lib/auth.tsx
@@ -6,7 +6,7 @@ const supabaseUrl = 'https://qwavakkbfpdgkvtctogx.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3YXZha2tiZnBkZ2t2dGN0b2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3MTE4MjYsImV4cCI6MjA1ODI4NzgyNn0.Kdwo9ICmcsHPhK_On6G73ccSPkcEqzAg2BtvblhD8co';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-interface User {
+export interface User {
   id: string;
   username: string;
   name: string;

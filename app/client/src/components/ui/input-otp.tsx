37ea26e3a20ef45d14a08d90dbb2b8a9614f6f0a
Mohan sai reddy <mohansaireddy22@gmail.com>
2025-04-22 10:27:17 +0530

    Fix OTPInput import in input-otp component

--- app/client/src/components/ui/input-otp.tsx
+++ app/client/src/components/ui/input-otp.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { OTPInput, OTPInputContext } from "react-otp-input"
+import OTPInput, { OTPInputContext } from "react-otp-input"
 import { Dot } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -9,14 +9,14 @@ const InputOTP = React.forwardRef<
   React.InputHTMLAttributes<HTMLInputElement>
 >(({ className, type, ...props }, ref) => {
   return (
-    <input
+    <OTPInput
+      {...props}
       type={type}
       className={cn(
         "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       ref={ref}
-      {...props}
     />
   )
 })
